fd25de6b185abcc6e9754f3098e4e5a0388d4c7d
sahilsingla1508-arch <sahilsingla1508@gmail.com>
2026-08-17 11:07:28 +0530

    Update module docstring and exports

--- _init_.py
+++ _init_.py
@@ -1,7 +1,13 @@
 """
-MeshWeaver - Resource Monitoring & Gossip Track (Week 1)
+MeshWeaver - Resource Monitoring & Gossip Track (Week 1 & 2)
 """
 from .monitoring import ResourceStatus, ResourceMonitor
-from .gossip import GossipMessageBuilder
+from .gossip import GossipMessageBuilder, BackgroundMonitorLoop, GossipManager
 
-__all__ = ["ResourceStatus", "ResourceMonitor", "GossipMessageBuilder"]
+__all__ = [
+    "ResourceStatus", 
+    "ResourceMonitor", 
+    "GossipMessageBuilder", 
+    "BackgroundMonitorLoop", 
+    "GossipManager"
+]
