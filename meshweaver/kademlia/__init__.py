663e1d950435df1f65511286267ae8293594b7b7
VaradKod <varadkod@gmail.com>
2026-08-29 18:41:12 +0530

    Complete cryptographic identity security

--- meshweaver/kademlia/__init__.py
+++ meshweaver/kademlia/__init__.py
@@ -3,8 +3,17 @@
 
 from meshweaver.kademlia.bootstrap import BootstrapClient, BootstrapError  # noqa: F401
 from meshweaver.kademlia.discovery import PeerDiscovery  # noqa: F401
+from meshweaver.kademlia.identity import (  # noqa: F401
+    NodeKeyPair,
+    generate_keypair,
+    load_keypair,
+    node_id_from_keypair,
+    save_keypair,
+)
 from meshweaver.kademlia.load_selector import (  # noqa: F401
     LoadEntry,
     LoadSelector,
     SelectionResult,
 )
+from meshweaver.kademlia.signer import TaskSigner, canonical_payload  # noqa: F401
+from meshweaver.kademlia.verifier import SignatureVerifier  # noqa: F401
